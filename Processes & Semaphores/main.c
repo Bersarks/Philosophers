be392274b66483471970b8600980d8f7a528c0f4
Bersarks <omer.faruk.tokluoglu@hotmail.com>
2023-01-31 15:55:09 +0300

    Repository creation

---
+++ Processes & Semaphores/main.c
@@ -0,0 +1,20 @@
+#include "philosophers.h"
+
+int	write_error(char *str)
+{
+	printf("Error: %s\n", str);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_rules	rules;
+
+	if (argc != 5 && argc != 6)
+		return (write_error("Wrong amount of arguments"));
+	if (init_data(&rules, argv))
+		return (EXIT_FAILURE);
+	if (launcher(&rules))
+		return (write_error("There was an error creating the forks"));
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Philosophers
-Dining philosophers problem solution with ecole 42 project
+This program is a program made using C language and includes multi thread, processes, mutex and semaphor. The program was inspired by the Philosophers problem.
